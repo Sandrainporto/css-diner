30e11afa4fdfbf79dffb6f303f9fd4e469014c7e
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-08-19 23:57:29 +0300

    feat: add bundle

--- src/entities/cssBlock/inputBlock/model/colorInputValue.ts
+++
@@ -1,7 +0,0 @@
-import { getHighlightedClasses } from '../../../../shared/utils/highlighter-js';
-const textToHighlight = document.createElement('div');
-
-export function colorInputValue(inputField: HTMLInputElement): void {
-    textToHighlight.innerHTML = getHighlightedClasses(inputField.value);
-    document.querySelector('.absolute-code')?.append(textToHighlight);
-}
